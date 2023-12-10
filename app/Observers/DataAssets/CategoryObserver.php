716cc23843db986b0851e79975147369484fcd4a
Ahmad Ilham <angilham2@gmail.com>
2023-12-10 23:30:47 +0700

    add oberver

---
+++ app/Observers/DataAssets/CategoryObserver.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Observers\DataAssets;
+
+use App\Models\DataMaster\Category;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class CategoryObserver
+{
+    /**
+     * Handle the Category "created" event.
+     */
+    public function created(Category $category): void
+    {
+        // $category->deleted_hash = md5($category->created_at);
+        // $category->save();
+    }
+
+    /**
+     * Handle the Category "updated" event.
+     */
+    public function updated(Category $category): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "deleted" event.
+     */
+    public function deleted(Category $category): void
+    {
+
+        $category->deleted_hash = md5($category->deleted_at);
+        $category->save();
+    }
+
+    /**
+     * Handle the Category "restored" event.
+     */
+    public function restored(Category $category): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Category "force deleted" event.
+     */
+    public function forceDeleted(Category $category): void
+    {
+        //
+    }
+}
