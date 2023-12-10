e19198bc1bffb8ed75a57a7695f17e8dbf71e1c5
Ahmad Ilham <angilham2@gmail.com>
2023-12-10 23:29:42 +0700

    feat:unit add unit models

---
+++ app/Models/DataMaster/Unit.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models\DataMaster;
+
+use App\Models\DataAsset\FixedAsset;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Unit extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'units';
+    protected $fillable = [
+        'nama',
+    ];
+
+    public function fixedAssets(): HasMany
+    {
+        return $this->hasMany(FixedAsset::class, 'unit_id', 'id');
+    }
+}
