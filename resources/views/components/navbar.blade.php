bc78b43a2c4e13a8a2e49862d55fac87552302af
Ahmad Ilham <angilham2@gmail.com>
2023-12-10 23:31:48 +0700

    add user-detail acces authorization

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -57,7 +57,7 @@
                         <div class="dropdown-divider"></div>
                     </li>
                     <li>
-                        <a class="dropdown-item" href="{{ route('user.detail') }}">
+                        <a class="dropdown-item" href="{{ route('user.detail', Auth::user()->id) }}">
                             <i class="bx bx-user me-2"></i>
                             <span class="align-middle">My Profile</span>
                         </a>
