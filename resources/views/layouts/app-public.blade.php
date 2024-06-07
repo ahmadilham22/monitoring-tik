ce753afdd4dda26d0012ef7ea44842788d8b6dad
Ahmad Ilham <angilham2@gmail.com>
2024-06-07 16:07:28 +0700

    feat: add public access

---
+++ resources/views/layouts/app-public.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default"
+    data-assets-path="../assets/" data-template="vertical-menu-template-free">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport"
+        content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Dashboard - Inventory</title>
+
+    <meta name="description" content="" />
+    @include('components.style')
+    @yield('css')
+    @include('sweetalert::alert')
+</head>
+
+<body>
+    <!-- Layout wrapper -->
+    <div class="layout-wrapper layout-content-navbar">
+        <div class="layout-container">
+            <!-- Menu -->
+            {{-- @include('components.sidebar') --}}
+            <!-- / Menu -->
+
+            <!-- Layout container -->
+            {{-- <div class="layout-page"> --}}
+            <!-- Navbar -->
+            {{-- @include('components.navbar') --}}
+
+            <!-- / Navbar -->
+
+            <!-- Content wrapper -->
+            <div class="content-wrapper">
+                <!-- Content -->
+                @yield('content')
+                <!-- / Content -->
+
+                <!-- Footer -->
+                {{-- @include('components.footer') --}}
+                <!-- / Footer -->
+            </div>
+            <!-- Content wrapper -->
+            {{-- </div> --}}
+            <!-- / Layout page -->
+        </div>
+    </div>
+    @include('components.script')
+    @yield('js')
+    @stack('addon-script')
+    @include('components.script-ajax-error')
+</body>
+
+</html>
